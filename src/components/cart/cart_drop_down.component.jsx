2c37f567459ae33bef5d326bc8e68a576257caf9
Slaven Antic <slaven.antic@gmail.com>
2021-09-18 20:12:34 +0200

    added selectors for cart

--- src/components/cart/cart_drop_down.component.jsx
+++ src/components/cart/cart_drop_down.component.jsx
@@ -3,10 +3,11 @@ import { CustomButton } from "../custom_button/custom_button.component";
 import "./cart_drop_down.styles.scss";
 import { connect } from "react-redux";
 import { CartItem } from "../cart_item/cart_item.component";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
-const mapStateToProps = ({ cart: { cartItems } }) => {
+const mapStateToProps = (state) => {
   return {
-    cartItems: cartItems,
+    cartItems: selectCartItems(state),
   };
 };
 
--- src/components/cart_icon/cart_icon.component.jsx
+++ src/components/cart_icon/cart_icon.component.jsx
@@ -3,6 +3,7 @@ import "./cart_icon.styles.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import * as cartActions from "../../redux/cart/cart.actions";
 import { connect } from "react-redux";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -10,13 +11,22 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-const CartIconComponent = ({ toggleCartHidden }) => {
+const mapStateToProps = (state) => {
+  return {
+    cartItemsCount: selectCartItemsCount(state),
+  };
+};
+
+const CartIconComponent = ({ toggleCartHidden, cartItemsCount }) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartItemsCount}</span>
     </div>
   );
 };
 
-export const CartIcon = connect(null, mapDispatchToProps)(CartIconComponent);
+export const CartIcon = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CartIconComponent);
